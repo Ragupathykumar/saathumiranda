c428398f57b1fabe32d0746b62e150b683d9bb34
Ragupathykunar <kragu892@gmail.com>
2022-05-24 08:52:08 +0530

    A changes

--- src/org/master/Mastered.java
+++ src/org/master/Mastered.java
@@ -6,12 +6,12 @@ public class Mastered {
 		for (int i = 1; i <= 5; i++) {
 			for (int j =5-i; j >=1 ; j--) {
 				System.out.print(" ");
-				
+				System.out.println();
 			}
 			for (int j = 1; j <= i; j++) {
 				System.out.print("* ");
 			}
 			System.out.println();
-
+System.out.println();
 }
 }}
